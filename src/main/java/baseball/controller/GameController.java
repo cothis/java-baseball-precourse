afa78b7b1632782654e7d8a1af42fe485be5b06a
cothis <minho1030@naver.com>
2022-04-18 00:29:48 +0900

    feat(GameController): 게임 컨트롤러 클래스

---
+++ src/main/java/baseball/controller/GameController.java
@@ -0,0 +1,38 @@
+package baseball.controller;
+
+import baseball.model.Balls;
+import baseball.model.TryResult;
+import baseball.view.GameView;
+
+public class GameController {
+    public void main() {
+        this.start();
+        while (GameView.requestRestartOrExit() == GameView.RESTART) {
+            this.start();
+        }
+        GameView.printExit();
+    }
+
+    private void start() {
+        final Balls answerBalls = Balls.createByRandoms();
+        TryResult result = null;
+        while(isPlaying(result)) {
+            final Balls userBalls = this.userBalls();
+            result = new TryResult(answerBalls, userBalls);
+            GameView.printResult(result);
+        }
+        GameView.printCompleteMessage();
+    }
+
+    private boolean isPlaying(TryResult result) {
+        if (result == null) {
+            return true;
+        }
+
+        return !result.isComplete();
+    }
+
+    private Balls userBalls() {
+        return Balls.createByUserInput(GameView.requestInsertNumbers());
+    }
+}
